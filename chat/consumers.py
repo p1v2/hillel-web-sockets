bfb92bb9ef0e72673f81b3566341bbe28a2fe131
VsevolodSheyko <dyeji9iht@gmail.com>
2023-05-29 13:26:48 +0300

    Create functions to see active users

--- chat/consumers.py
+++ chat/consumers.py
@@ -4,6 +4,7 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
+    connected_users = 0
     async def connect(self):
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = "chat_%s" % self.room_name
@@ -11,11 +12,17 @@ class ChatConsumer(AsyncWebsocketConsumer):
         # Join room group
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
 
+        ChatConsumer.connected_users += 1
+        await self.channel_layer.group_send(self.room_group_name, {"type": "user_count", "connected_users": ChatConsumer.connected_users})
+
         await self.accept()
 
     async def disconnect(self, close_code):
         # Leave room group
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+
+        ChatConsumer.connected_users -= 1
+        await self.channel_layer.group_send(self.room_group_name,{"type": "user_count", "connected_users": ChatConsumer.connected_users})
 
     # Receive message from WebSocket
     async def receive(self, text_data=None, bytes_data=None):
@@ -36,3 +43,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
         # Send message to WebSocket
         await self.send(text_data=json.dumps({"message": message, "name": name}))
+    
+    async def user_count(self, event):
+        connected_users = event["connected_users"]
+        await self.send(text_data=json.dumps({"connected_users": connected_users}))
